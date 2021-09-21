106d85fd9552cfb923852982f2441abfe98cd2cb
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-09-20 22:28:55 -0700

    commit tree d1b52841fa201e0e28e9b2efe95e06dabdc42b2e

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
+let num1 =  31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
+
+let random = Math.ceil(Math.random() + 1)
+console.log(random)
+
+let num3 = 25
+let num4 = 7
+let mod = num3 % num4
+console.log(mod)
+
+let max = Math.max(6,7,20,19,15,12,3)
+console.log(max)
 // Write your code here
